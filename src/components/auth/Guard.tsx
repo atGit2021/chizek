398b434a9a4b0b2291f6b785e9c5f715d48cb71f
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-02-07 20:10:22 -0600

    New guard to protect routes (#6)
    * New guard to protect routes
    
    * clean up lint issue

---
+++ src/components/auth/Guard.tsx
@@ -0,0 +1,20 @@
+import excludedRoutes from "../../constants/excluded-routes";
+import { useGetCurrentUser } from "../../hooks/useGetStoredUser";
+
+interface GuardProps {
+  children: JSX.Element;
+}
+
+const Guard = ({ children }: GuardProps) => {
+  const { data: user } = useGetCurrentUser();
+  console.log(user);
+  return (
+    <>
+      {excludedRoutes.includes(window.location.pathname)
+        ? children
+        : user && children}
+    </>
+  );
+};
+
+export default Guard;
