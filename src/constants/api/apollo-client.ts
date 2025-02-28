d1fe1b0d7324050d7ed091ee791be778fbe45708
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-02-28 12:45:15 -0600

    Enabled logout functionality (#12)

--- src/constants/api/apollo-client.ts
+++ src/constants/api/apollo-client.ts
@@ -1,26 +1,25 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { onError } from "@apollo/client/link/error"
+import { onError } from "@apollo/client/link/error";
 import { API_URL } from "./urls";
 import excludedRoutes from "../excluded-routes";
-
-const logoutEvent = new Event("logout");
+import { onLogout } from "../../utils/onLogout";
 
 const logoutLink = onError((error) => {
-    if (
-        error.graphQLErrors?.length && 
-        (error.graphQLErrors[0].extensions?.originalError as any).statusCode === 401
-    ) {
-        if (!excludedRoutes.includes(window.location.pathname)) {
-           window.dispatchEvent(logoutEvent);
-        }
+  if (
+    error.graphQLErrors?.length &&
+    (error.graphQLErrors[0].extensions?.originalError as any).statusCode === 401
+  ) {
+    if (!excludedRoutes.includes(window.location.pathname)) {
+      onLogout();
     }
+  }
 });
 
 const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
 
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: logoutLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  link: logoutLink.concat(httpLink),
 });
 
 export default client;
