6d12b005603d7e9687c0d4bb8ffec4f2bde55d8b
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-03-18 15:49:13 -0500

    clean up lint issues (#23)

--- src/utils/extractErrorMessage.ts
+++ src/utils/extractErrorMessage.ts
@@ -1,11 +1,23 @@
-const extractErrorMessage = (err: any) => {
-    const errorMessage = err.graphQLErrors[0]?.extensions?.originalError?.message;
-    if (!errorMessage) return '';
-    return formatErrorMessage(Array.isArray(errorMessage) ? errorMessage[0] : errorMessage);
+interface GraphQLError {
+  graphQLErrors: {
+    extensions?: {
+      originalError?: {
+        message: string | string[];
+      };
+    };
+  }[];
+}
+
+const extractErrorMessage = (err: GraphQLError) => {
+  const errorMessage = err.graphQLErrors[0]?.extensions?.originalError?.message;
+  if (!errorMessage) return "";
+  return formatErrorMessage(
+    Array.isArray(errorMessage) ? errorMessage[0] : errorMessage
+  );
 };
 
 const formatErrorMessage = (errorMessage: string) => {
-    return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
-}
+  return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
+};
 
 export { extractErrorMessage };
