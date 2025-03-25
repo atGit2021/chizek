c1f8074fc9b6454772f11fdaa8049f140a25ba03
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-03-24 20:27:05 -0500

    Chat modal (#29)
    * Add chat forum modal
    
    * Added switch and textfield to modal

---
+++ src/components/chat-list/chat-list-modal/ChatListAdd.tsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  InputBase,
+  Modal,
+  Paper,
+  Stack,
+  Switch,
+  TextField,
+  Typography,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { MouseEvent, useState } from 'react';
+
+interface ChatListAddProps {
+  open: boolean;
+  handleClose: () => void;
+}
+
+const ChatListAdd = ({ open, handleClose }: ChatListAddProps) => {
+  const [isPrivate, setIsPrivate] = useState(true);
+  const handleModalClick = (event: MouseEvent) => {
+    event.stopPropagation();
+  };
+
+  return (
+    <Modal open={open} onClick={handleClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          bgcolor: 'background.paper',
+          border: '2px solid #040',
+          boxShadow: 24,
+          p: 4,
+        }}
+        onClick={handleModalClick}
+      >
+        <Stack spacing={2}>
+          <Typography variant="h6" component="h2">
+            Add Chat
+          </Typography>
+          <FormGroup>
+            <FormControlLabel
+              style={{ width: 0 }}
+              control={
+                <Switch
+                  checked={isPrivate}
+                  onChange={(event) => setIsPrivate(event.target.checked)}
+                />
+              }
+              label="Private"
+            ></FormControlLabel>
+          </FormGroup>
+          {isPrivate ? (
+            <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}>
+              <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search Users" />
+              <IconButton sx={{ p: '10px' }}>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          ) : (
+            <TextField label="Name" />
+          )}
+          <Button variant="outlined">Save</Button>
+        </Stack>
+      </Box>
+    </Modal>
+  );
+};
+
+export default ChatListAdd;
