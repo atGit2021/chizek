9255ec0db3c76ce2f9446a83a76487737c46350c
atGit2021 <a.c.turner.gm@gmail.com>
2025-04-02 12:04:21 -0500

    revised hook to update cache after mutation

--- src/hooks/useCreateForum.ts
+++ src/hooks/useCreateForum.ts
@@ -1,5 +1,6 @@
 import { UNKNOWN_ERROR_SNACK_MESSAGE } from '../constants/errors';
 import { snackVar } from '../constants/snack';
+import { ForumFragment } from '../fragments/forum.fragment';
 import { graphql } from '../gql';
 import { useMutation } from '@apollo/client';
 
@@ -13,6 +14,19 @@ const createForumDocument = graphql(`
 
 const useCreateForum = () => {
   return useMutation(createForumDocument, {
+    update: (cache, { data }) => {
+      cache.modify({
+        fields: {
+          forums(existingForums = []) {
+            const newForumRef = cache.writeFragment({
+              data: data?.createForum,
+              fragment: ForumFragment,
+            });
+            return [...existingForums, newForumRef];
+          },
+        },
+      });
+    },
     onError: (error) => {
       if (error.networkError) {
         snackVar(UNKNOWN_ERROR_SNACK_MESSAGE);
