a6288c64cc0285ca420ee45e04fd831d3f96e688
atGit2021 <a.c.turner.gm@gmail.com>
2025-04-03 18:38:55 -0500

    clear session storage on logout

--- src/utils/onLogout.ts
+++ src/utils/onLogout.ts
@@ -3,8 +3,8 @@ import { authenticatedVar } from '../constants/authenticated';
 import router from '../routes/Routes';
 
 export const onLogout = async () => {
-  authenticatedVar(false);
   sessionStorage.removeItem('authenticated');
+  authenticatedVar(false);
   await client.clearStore();
   router.navigate('/login');
 };
