26fe5c3138f264b7153eeb0d9c6e7aad878a8791
atGit2021 <a.c.turner.gm@gmail.com>
2025-04-18 10:37:32 -0500

    Modified cache to update with latest message

--- src/hooks/useCreateMessage.ts
+++ src/hooks/useCreateMessage.ts
@@ -1,5 +1,8 @@
 import { useMutation } from '@apollo/client';
 import { graphql } from '../gql';
+import { snackVar } from '../constants/snack';
+import { UNKNOWN_ERROR_SNACK_MESSAGE } from '../constants/errors';
+import { MessageFragmentFragmentDoc } from '../gql/graphql';
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
@@ -10,7 +13,31 @@ const createMessageDocument = graphql(`
 `);
 
 const useCreateMessage = () => {
-  return useMutation(createMessageDocument);
+  return useMutation(createMessageDocument, {
+    update: (cache, { data }) => {
+      cache.modify({
+        fields: {
+          messages(existingMessages = []) {
+            const newMessageRef = cache.writeFragment({
+              data: data?.createMessage,
+              fragment: MessageFragmentFragmentDoc,
+            });
+            return [...existingMessages, newMessageRef];
+          },
+        },
+      });
+    },
+    onError: (error) => {
+      if (error.networkError) {
+        snackVar(UNKNOWN_ERROR_SNACK_MESSAGE);
+      }
+      if (error.graphQLErrors) {
+        error.graphQLErrors.forEach(({ message }) => {
+          snackVar({ message, type: 'error' });
+        });
+      }
+    },
+  });
 };
 
 export { useCreateMessage };
