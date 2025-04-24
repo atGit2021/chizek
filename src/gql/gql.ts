7537939be8573504fb4d8be7782e15f7c9dec984
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-04-24 18:26:05 -0500

    refactor graphql forum queries and mutation (#54)

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -14,26 +14,35 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 type Documents = {
-    "\n  fragment ForumFragment on Forum {\n    _id\n    name\n    userId\n    isPrivate\n    userIds\n  }\n": typeof types.ForumFragmentFragmentDoc,
-    "fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}": typeof types.MessageFragmentFragmentDoc,
-    "query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}": typeof types.MessagesDocument,
-    "\n  mutation createForum($createForumInput: CreateForumInput!) {\n    createForum(createForumInput: $createForumInput) {\n      ...ForumFragment\n    }\n  }\n": typeof types.CreateForumDocument,
-    "\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n": typeof types.CreateMessageDocument,
-    "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n": typeof types.CreateUserDocument,
-    "\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n": typeof types.GetCurrentUserDocument,
-    "\n  query forum($_id: String!) {\n    forum(_id: $_id) {\n      ...ForumFragment\n    }\n  }\n": typeof types.ForumDocument,
-    "\n  query forums {\n    forums {\n      ...ForumFragment\n    }\n  }\n": typeof types.ForumsDocument,
+  'fragment ForumFragment on Forum {\n  _id\n  name\n  userId\n  isPrivate\n  userIds\n}': typeof types.ForumFragmentFragmentDoc;
+  'fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}': typeof types.MessageFragmentFragmentDoc;
+  'mutation CreateForum($createForumInput: CreateForumInput!) {\n  createForum(createForumInput: $createForumInput) {\n    ...ForumFragment\n  }\n}': typeof types.CreateForumDocument;
+  'query Forum($_id: String!) {\n  forum(_id: $_id) {\n    ...ForumFragment\n  }\n}': typeof types.ForumDocument;
+  'query Forums {\n  forums {\n    ...ForumFragment\n  }\n}': typeof types.ForumsDocument;
+  'query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}': typeof types.MessagesDocument;
+  '\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n': typeof types.CreateMessageDocument;
+  '\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n': typeof types.CreateUserDocument;
+  '\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n': typeof types.GetCurrentUserDocument;
 };
 const documents: Documents = {
-    "\n  fragment ForumFragment on Forum {\n    _id\n    name\n    userId\n    isPrivate\n    userIds\n  }\n": types.ForumFragmentFragmentDoc,
-    "fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}": types.MessageFragmentFragmentDoc,
-    "query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}": types.MessagesDocument,
-    "\n  mutation createForum($createForumInput: CreateForumInput!) {\n    createForum(createForumInput: $createForumInput) {\n      ...ForumFragment\n    }\n  }\n": types.CreateForumDocument,
-    "\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n": types.CreateMessageDocument,
-    "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n": types.CreateUserDocument,
-    "\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n": types.GetCurrentUserDocument,
-    "\n  query forum($_id: String!) {\n    forum(_id: $_id) {\n      ...ForumFragment\n    }\n  }\n": types.ForumDocument,
-    "\n  query forums {\n    forums {\n      ...ForumFragment\n    }\n  }\n": types.ForumsDocument,
+  'fragment ForumFragment on Forum {\n  _id\n  name\n  userId\n  isPrivate\n  userIds\n}':
+    types.ForumFragmentFragmentDoc,
+  'fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}':
+    types.MessageFragmentFragmentDoc,
+  'mutation CreateForum($createForumInput: CreateForumInput!) {\n  createForum(createForumInput: $createForumInput) {\n    ...ForumFragment\n  }\n}':
+    types.CreateForumDocument,
+  'query Forum($_id: String!) {\n  forum(_id: $_id) {\n    ...ForumFragment\n  }\n}':
+    types.ForumDocument,
+  'query Forums {\n  forums {\n    ...ForumFragment\n  }\n}':
+    types.ForumsDocument,
+  'query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}':
+    types.MessagesDocument,
+  '\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n':
+    types.CreateMessageDocument,
+  '\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n':
+    types.CreateUserDocument,
+  '\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n':
+    types.GetCurrentUserDocument,
 };
 
 /**
@@ -53,42 +62,61 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  fragment ForumFragment on Forum {\n    _id\n    name\n    userId\n    isPrivate\n    userIds\n  }\n"): (typeof documents)["\n  fragment ForumFragment on Forum {\n    _id\n    name\n    userId\n    isPrivate\n    userIds\n  }\n"];
+export function graphql(
+  source: 'fragment ForumFragment on Forum {\n  _id\n  name\n  userId\n  isPrivate\n  userIds\n}',
+): (typeof documents)['fragment ForumFragment on Forum {\n  _id\n  name\n  userId\n  isPrivate\n  userIds\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}"): (typeof documents)["fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}"];
+export function graphql(
+  source: 'fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}',
+): (typeof documents)['fragment MessageFragment on Message {\n  _id\n  content\n  createdAt\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}"): (typeof documents)["query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}"];
+export function graphql(
+  source: 'mutation CreateForum($createForumInput: CreateForumInput!) {\n  createForum(createForumInput: $createForumInput) {\n    ...ForumFragment\n  }\n}',
+): (typeof documents)['mutation CreateForum($createForumInput: CreateForumInput!) {\n  createForum(createForumInput: $createForumInput) {\n    ...ForumFragment\n  }\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  mutation createForum($createForumInput: CreateForumInput!) {\n    createForum(createForumInput: $createForumInput) {\n      ...ForumFragment\n    }\n  }\n"): (typeof documents)["\n  mutation createForum($createForumInput: CreateForumInput!) {\n    createForum(createForumInput: $createForumInput) {\n      ...ForumFragment\n    }\n  }\n"];
+export function graphql(
+  source: 'query Forum($_id: String!) {\n  forum(_id: $_id) {\n    ...ForumFragment\n  }\n}',
+): (typeof documents)['query Forum($_id: String!) {\n  forum(_id: $_id) {\n    ...ForumFragment\n  }\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n"): (typeof documents)["\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n"];
+export function graphql(
+  source: 'query Forums {\n  forums {\n    ...ForumFragment\n  }\n}',
+): (typeof documents)['query Forums {\n  forums {\n    ...ForumFragment\n  }\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n"): (typeof documents)["\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n"];
+export function graphql(
+  source: 'query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}',
+): (typeof documents)['query Messages($forumId: String!) {\n  messages(forumId: $forumId) {\n    ...MessageFragment\n  }\n}'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n"): (typeof documents)["\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n"];
+export function graphql(
+  source: '\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n',
+): (typeof documents)['\n  mutation CreateMessage($createMessageInput: CreateMessageInput!) {\n    createMessage(createMessageInput: $createMessageInput) {\n      ...MessageFragment\n    }\n  }\n'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query forum($_id: String!) {\n    forum(_id: $_id) {\n      ...ForumFragment\n    }\n  }\n"): (typeof documents)["\n  query forum($_id: String!) {\n    forum(_id: $_id) {\n      ...ForumFragment\n    }\n  }\n"];
+export function graphql(
+  source: '\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n',
+): (typeof documents)['\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n'];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query forums {\n    forums {\n      ...ForumFragment\n    }\n  }\n"): (typeof documents)["\n  query forums {\n    forums {\n      ...ForumFragment\n    }\n  }\n"];
+export function graphql(
+  source: '\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n',
+): (typeof documents)['\n  query getCurrentUser {\n    getCurrentUser {\n      _id\n      email\n    }\n  }\n'];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
