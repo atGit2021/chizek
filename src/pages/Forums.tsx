0a0df2c416befaec36a4cd903a7cc1f2e2a11586
atGit2021 <a.c.turner.gm@gmail.com>
2025-06-13 14:52:12 -0500

    refactor forums and cleaned up styling

--- src/pages/Forums.tsx
+++ src/pages/Forums.tsx
@@ -1,22 +1,32 @@
 import Forum from '../components/forum/Forum';
-import ForumList from '../components/forum-list/ForumList';
 import Grid from '@mui/material/Grid2';
-import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useGetForums } from '../hooks/useGetForums';
 import { PAGE_SIZE } from '../constants/page-size';
 import { useCountForums } from '../hooks/useCountForums';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Box, Divider, Stack } from '@mui/material';
+import ForumListItem from '../components/forum-list/forum-list-item/ForumListItem';
+import ForumListHeader from '../components/forum-list/forum-list-header/ForumListHeader';
+import ForumListAdd from '../components/forum-list/forum-list-modal/ForumListAdd';
+import { useMessageCreated } from '../hooks/useMessageCreated';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const Forums = () => {
   const { data, fetchMore } = useGetForums({ skip: 0, limit: PAGE_SIZE });
   const { forumsCount, countForums } = useCountForums();
   const params = useParams();
   const forumIdSelected = params._id ?? undefined;
+  const [forumListAddVisible, setForumListAddVisible] = useState(false);
   let forumSelected = null;
+
   if (forumIdSelected) {
     forumSelected = data?.forums.find((forum) => forum._id === forumIdSelected);
   }
+
+  useMessageCreated({
+    forumIds: data?.forums.map((forum) => forum._id) || [],
+  });
 
   useEffect(() => {
     countForums();
@@ -24,20 +34,65 @@ const Forums = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid size={{ md: 3 }}>
-        <ForumList
-          forums={data}
-          fetchMore={fetchMore}
-          forumsCount={forumsCount}
-        />
+      <Grid
+        sx={{ gridColumn: { xs: 'span 12', md: 'span 3' }, height: '100%' }}
+      >
+        <Stack>
+          <ForumListHeader
+            handleAddForum={() => setForumListAddVisible(true)}
+          />
+          <Divider />
+          <Box
+            sx={{
+              width: '100%',
+              bgcolor: 'background.paper',
+              maxHeight: '80vh',
+              overflow: 'auto',
+            }}
+          >
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={() =>
+                fetchMore({
+                  variables: {
+                    skip: data?.forums.length || 0,
+                    limit: PAGE_SIZE,
+                  },
+                })
+              }
+              hasMore={
+                data?.forums && forumsCount
+                  ? forumsCount > data?.forums.length
+                  : false
+              }
+              useWindow={false}
+            >
+              {data?.forums &&
+                [...data.forums]
+                  .sort((forumA, forumB) => {
+                    if (!forumA.latestMessage) return 1;
+                    if (!forumB.latestMessage) return -1;
+                    return (
+                      new Date(forumB.latestMessage.createdAt).getTime() -
+                      new Date(forumA.latestMessage.createdAt).getTime()
+                    );
+                  })
+                  .map((forum) => (
+                    <ForumListItem key={forum._id} forum={forum} />
+                  ))}
+            </InfiniteScroll>
+          </Box>
+        </Stack>
       </Grid>
-      <Grid size={{ md: 9 }}>
-        {forumSelected ? (
-          <Forum forum={forumSelected} />
-        ) : (
-          <Typography>Forum News</Typography>
-        )}
+      <Grid
+        sx={{ gridColumn: { xs: 'span 12', md: 'span 9' }, height: '100%' }}
+      >
+        {forumSelected && <Forum forum={forumSelected} />}
       </Grid>
+      <ForumListAdd
+        open={forumListAddVisible}
+        handleClose={() => setForumListAddVisible(false)}
+      />
     </Grid>
   );
 };
